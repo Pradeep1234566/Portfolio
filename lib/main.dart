302d0f734c7afab953da2ebf17a07b086473fc99
Pradeep <pawarpradeep1707@gmail.com>
2025-02-06 20:26:42 +0530

    This looks good portfolio man the commit messages are weird

--- lib/main.dart
+++ lib/main.dart
@@ -10,62 +10,89 @@ class MyPortfolio extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PhoneHomeScreen(),
+      home: WebPortfolioScreen(),
     );
   }
 }
 
-class PhoneHomeScreen extends StatelessWidget {
+class WebPortfolioScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          // Status Bar
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-            alignment: Alignment.centerRight,
-            child: Text(
-              "12:30 PM",
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
-          ),
-          // Home Screen with App Icons
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 3,
-              padding: EdgeInsets.all(20),
-              children: [
-                _buildAppIcon(Icons.article, "Resume", 'https://drive.google.com/file/d/1BO0JIw7JQNliLNma5WmgMrDh-iU5tbBy/view?usp=sharing'),
-                _buildAppIcon(Icons.work, "Projects", 'https://github.com/Pradeep1234566'),
-                _buildAppIcon(Icons.code, "Tech Stack", ''),
-                _buildAppIcon(Icons.star, "Achievements", ''),
-                _buildAppIcon(Icons.school, "Education", ''),
-                _buildAppIcon(Icons.contact_mail, "Contact", ''),
-              ],
-            ),
-          ),
-          // Dock Bar
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 10),
-            decoration: BoxDecoration(
-              color: Colors.white10,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Pradeep's Portfolio",
+                style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
               ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.home, color: Colors.white, size: 30),
-                Icon(Icons.search, color: Colors.white, size: 30),
-                Icon(Icons.settings, color: Colors.white, size: 30),
-              ],
-            ),
+              SizedBox(height: 20),
+              // Phone Mockup
+              Container(
+                width: 300,
+                height: 600,
+                decoration: BoxDecoration(
+                  color: Colors.white10,
+                  borderRadius: BorderRadius.circular(40),
+                  border: Border.all(color: Colors.white, width: 3),
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(height: 20),
+                    Text("12:30 PM",
+                        style: TextStyle(color: Colors.white, fontSize: 18)),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: GridView.count(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                          shrinkWrap: true,
+                          children: [
+                            _buildAppIcon(Icons.article, "Resume",
+                                'https://drive.google.com/file/d/1BO0JIw7JQNliLNma5WmgMrDh-iU5tbBy/view?usp=sharing'),
+                            _buildAppIcon(Icons.work, "Projects",
+                                'https://github.com/Pradeep1234566'),
+                            _buildAppIcon(Icons.code, "Tech Stack", ''),
+                            _buildAppIcon(Icons.star, "Achievements", ''),
+                            _buildAppIcon(Icons.school, "Education", ''),
+                            _buildAppIcon(Icons.contact_mail, "Contact", ''),
+                          ],
+                        ),
+                      ),
+                    ),
+                    // Dock Bar
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.white10,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(40),
+                          bottomRight: Radius.circular(40),
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(Icons.home, color: Colors.white, size: 30),
+                          Icon(Icons.search, color: Colors.white, size: 30),
+                          Icon(Icons.settings, color: Colors.white, size: 30),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
